0b1ffc5e61a879c2cda696d5b50d21bf1b544409
hmalti <hmalti@polytech-lille.net>
2020-03-22 20:08:51 +0100

    modifs CC, test shm + début serveur web

--- CC/commtcp.c
+++ CC/commtcp.c
@@ -10,24 +10,43 @@
 #include <libnetwork.h>
 
 // function for chat
-void *traitement(int s)
+/* void *traitement(int s)
 {
     char *hello = "Hello from CC";
     write(s, (void *)hello, strlen(hello));
     return 0;
-}
+} */
 
-/* int main()
+int main()
 {
-    //Msg à envoyer à tout le monde en TCP
-    //char *hello = "Hello from CC";
-    int s;
+
+    // PARTIE SERVEUR UDP (écoute) 
+
+    int s1 = initialisationServeurUDP("2020");
+
+    if (s1 < 0)
+    {
+        fprintf(stderr, "Initialisation du serveur UDP impossible\n");
+        exit(-1);
+    }
+
+    /* Lancement de la boucle d'ecoute */
+    if (boucleServeurUDP(s1, nouveauClient) < 0)
+    {
+        fprintf(stderr, "Connexion avec le client UDP impossible\n");
+        exit(-1);
+    }
+
+
+    //PARTIE Client TCP (envoie)
+    char *hello = "Hello from CC";
+    int s2;
     // Structure addresse du serveur (comme en UDP)
     struct sockaddr_in servaddr;
 
-    s = socket(AF_INET, SOCK_STREAM, 0);
+    s2 = socket(AF_INET, SOCK_STREAM, 0);
     //Création de la socket : s = file descriptor de la socket, AF_INET (socket internet), SOCK_DGRAM (datagramme, UDP, sans connexion)
-    if (s < 0)
+    if (s2 < 0)
     {
         //Test de la valeur de retour de la socket
         perror("sendUDPBroadcast.socket");
@@ -39,20 +58,19 @@ void *traitement(int s)
     servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     //Pareil pour le port
     servaddr.sin_port = htons(2020);
-   
-        if (connect(s, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
-        {
-            printf("connection with the server failed...\n");
-            exit(0);
-        }
-        else
-        {
-            printf("connected to the server..\n");
-            boucleServeurTCP(s, traitement(s));
-            // close the socket
-            
-        }
-    close(s);
+
+    if (connect(s2, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        printf("connection with the server failed...\n");
+        exit(0);
+    }
+    else
+    {
+        printf("connected to the server..\n");
+        //boucleServeurTCP(s, traitement(s));
+        sendTCP(s2,hello, strlen(hello));
+        // close the socket
+    }
+    close(s2);
     return 0;
 }
- */
