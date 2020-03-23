51df1f945798ad0169db0a9330c7e6ebe36f4b42
hmalti <hmalti@polytech-lille.net>
2020-03-23 12:52:32 +0100

    boucle écoute udp qui prinf le msg envoyé OK

--- CC/commtcp.c
+++ CC/commtcp.c
@@ -9,6 +9,8 @@
 #include <netinet/ip.h>
 #include <libnetwork.h>
 
+#define UDP_PORT_ECOUTE "4242"
+#define IP_BOT "127.0.0.1"
 // function for chat
 /* void *traitement(int s)
 {
@@ -17,36 +19,46 @@
     return 0;
 } */
 
+int traitement_udp(unsigned char *message, int taille)
+{
+    (void)taille;
+    printf("le msg que j'ai reçu du bot: %s\n", (char *)message);
+
+    return 0;
+}
+
 int main()
 {
 
-    // PARTIE SERVEUR UDP (écoute) 
+    // PARTIE SERVEUR UDP (écoute)
+    char *port_udp = UDP_PORT_ECOUTE;
 
-    int s1 = initialisationServeurUDP("2020");
+    int socket_udp = initialisationServeurUDP(port_udp);
 
-    if (s1 < 0)
+    if (socket_udp < 0)
     {
         fprintf(stderr, "Initialisation du serveur UDP impossible\n");
         exit(-1);
     }
-
+    //TO DO : lancement dans un thread
     /* Lancement de la boucle d'ecoute */
-    if (boucleServeurUDP(s1, nouveauClient) < 0)
+    //if (boucleServeurUDP(socket_udp, traitement_udp  < 0) Faux car nom_a_la
+    if (boucleServeurUDP(socket_udp, traitement_udp) < 0)
     {
         fprintf(stderr, "Connexion avec le client UDP impossible\n");
         exit(-1);
     }
-
+    close(socket_udp);
 
     //PARTIE Client TCP (envoie)
     char *hello = "Hello from CC";
-    int s2;
+    int socket_tcp;
     // Structure addresse du serveur (comme en UDP)
     struct sockaddr_in servaddr;
 
-    s2 = socket(AF_INET, SOCK_STREAM, 0);
+    socket_tcp = socket(AF_INET, SOCK_STREAM, 0);
     //Création de la socket : s = file descriptor de la socket, AF_INET (socket internet), SOCK_DGRAM (datagramme, UDP, sans connexion)
-    if (s2 < 0)
+    if (socket_tcp < 0)
     {
         //Test de la valeur de retour de la socket
         perror("sendUDPBroadcast.socket");
@@ -55,11 +67,11 @@ int main()
 
     servaddr.sin_family = AF_INET;
     //On met l'addresse du serveur avec qui on veut communiquer
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr.sin_addr.s_addr = inet_addr(IP_BOT);
     //Pareil pour le port
     servaddr.sin_port = htons(2020);
 
-    if (connect(s2, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    if (connect(socket_tcp, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
     {
         printf("connection with the server failed...\n");
         exit(0);
@@ -68,9 +80,9 @@ int main()
     {
         printf("connected to the server..\n");
         //boucleServeurTCP(s, traitement(s));
-        sendTCP(s2,hello, strlen(hello));
+        sendTCP(socket_tcp, hello, strlen(hello));
         // close the socket
     }
-    close(s2);
+    close(socket_tcp);
     return 0;
 }
