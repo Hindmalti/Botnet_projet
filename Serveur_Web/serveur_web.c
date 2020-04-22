669ad4b6564a1a080dabd412abb8e708938434c7
lahouass <davylorisahouassou@gmail.com>
2020-04-22 21:42:03 +0200

    ajout fonction création options de choix bot

--- Serveur_Web/serveur_web.c
+++ Serveur_Web/serveur_web.c
@@ -3,7 +3,6 @@
 
 /** Include files **/
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,15 +19,15 @@
 
 /** Some constants **/
 
-#define WEB_DIR  "./www"
+#define WEB_DIR "./www"
 #define PAGE_NOTFOUND "error.html"
 #define MAX_BUFFER 1024
 
-#define CODE_OK  200
+#define CODE_OK 200
 #define CODE_NOTFOUND 404
 
 #define NB_BOTS 3
-char* ListeBot[3] = {"Bot1", "Bot2", "Bot3"};
+char *ListeBot[3] = {"Bot1", "Bot2", "Bot3"};
 
 /* gestion des requêtes web 
 - prend en paramètre la socket de dialogue
@@ -36,10 +35,16 @@ char* ListeBot[3] = {"Bot1", "Bot2", "Bot3"};
 - 
 
 */
-void gestionClientWeb(void *s){
 
-    
-   
+char* options_bot(char* bot){
+    char* option;
+    sprintf(option,"<option value=\"%s\">%s</option>", bot, bot);
+    return option;
+}
+
+void gestionClientWeb(void *s)
+{
+
     char buffer[MAX_BUFFER];
     char cmd[MAX_BUFFER];
     char page[MAX_BUFFER];
@@ -55,146 +60,150 @@ void gestionClientWeb(void *s){
         perror("gestionClient.fdopen");
         exit(EXIT_FAILURE);
     }
-    
+
     //PageWeb d'acceuil chargée dynamiquement créant les bouton
     // Pour chaque bot présent dans la liste
 
-      //char liste[MAX_BUFFER];
-      //while(fgets(liste,MAX_BUFFER,dialogue)!=NULL){
-       
-        
+    //char liste[MAX_BUFFER];
+    //while(fgets(liste,MAX_BUFFER,dialogue)!=NULL){
+
     /* Termine la connexion */
-   
+
     //}
 
-    
     //Gestion des requêtes POST et GET
 
-    if(fgets(buffer,MAX_BUFFER,dialogue)==NULL) exit(-1);
-    if(sscanf(buffer,"%s %s %s",cmd,page,proto)!=3) exit(-1);
+    if (fgets(buffer, MAX_BUFFER, dialogue) == NULL)
+        exit(-1);
+    if (sscanf(buffer, "%s %s %s", cmd, page, proto) != 3)
+        exit(-1);
     printf("%s\n", buffer);
 
     int content_length = 0;
     char *temp = NULL;
-    
-    
-    while(fgets(buffer,MAX_BUFFER,dialogue)!=NULL){
-        printf("%s", buffer); // Pour afficher les header
-        if(strcmp(buffer,"\r\n")==0) break;
 
-        if((temp = strstr(buffer, "Content-Length:")) != NULL) {
-            content_length = atoi(temp+16); //On récupère la taille du fichier
-        } 
-    
-  }
-  printf("MON CONTENT LENGTH = %d\n", content_length);
-  char donnees[content_length];
-  if(content_length > 0) {
-        for(int i = 0; i < content_length; i++) {
+    while (fgets(buffer, MAX_BUFFER, dialogue) != NULL)
+    {
+        printf("%s", buffer); // Pour afficher les header
+        if (strcmp(buffer, "\r\n") == 0)
+            break;
+
+        if ((temp = strstr(buffer, "Content-Length:")) != NULL)
+        {
+            content_length = atoi(temp + 16); //On récupère la taille du fichier
+        }
+    }
+    printf("MON CONTENT LENGTH = %d\n", content_length);
+    char donnees[content_length];
+    if (content_length > 0)
+    {
+        for (int i = 0; i < content_length; i++)
+        {
             donnees[i] = fgetc(dialogue); //On récupère le contenu du fichier
         }
-    	donnees[content_length] = '\0';
-        } 
-        printf(" CONTENU DU FICHIER = %s\n", donnees);
-        printf("fin de mon contenu\n");
-
-  
-
-  //printf("%ld", fread(buffer,strlen(buffer)+1, 1, dialogue));
-  // 1) faire un malloc de la longueur de ma page (content-length) qui doit etre
-  // en unsigned char
-  // 2) recupérer la longueur avec un fread(buffer,0,bytes,dialogue)
-  // si > 0 alors fficher
-
-  
-  // Traitement des réquêtes POST et GET : adapté du serveur web C minimal
-  // le traitement de la requêtes post a été rajouté.
-  if(strcasecmp(cmd,"GET")==0 || strcasecmp(cmd,"POST")==0){
-  int code=CODE_OK;
-  struct stat fstat;
-  sprintf(path,"%s%s",WEB_DIR,page);
-  if(stat(path,&fstat)!=0 || !S_ISREG(fstat.st_mode)){
-    sprintf(path,"%s/%s",WEB_DIR,PAGE_NOTFOUND);
-    code=CODE_NOTFOUND;
+        donnees[content_length] = '\0';
     }
-  strcpy(type,"text/html");
-  char *end=page+strlen(page);
-  if(strcmp(end-4,".png")==0) strcpy(type,"image/png");
-  if(strcmp(end-4,".jpg")==0) strcpy(type,"image/jpg");
-  if(strcmp(end-4,".gif")==0) strcpy(type,"image/gif");
-  fprintf(dialogue,"HTTP/1.0 %d\r\n",code);
-  fprintf(dialogue,"Server: CWeb\r\n");
-  
-  fprintf(dialogue,"Content-type: %s\r\n",type);
-  fprintf(dialogue,"Content-length: %ld\r\n",fstat.st_size);
-  fprintf(dialogue,"\r\n");
-  fflush(dialogue);
-  int fd=open(path,O_RDONLY);
-  if(fd>=0){
-    int bytes;
-    while((bytes=read(fd,buffer,MAX_BUFFER))>0){
-        fwrite(buffer,1,bytes,dialogue);
+    printf(" CONTENU DU FICHIER = %s\n", donnees);
+    printf("fin de mon contenu\n");
+
+    //printf("%ld", fread(buffer,strlen(buffer)+1, 1, dialogue));
+    // 1) faire un malloc de la longueur de ma page (content-length) qui doit etre
+    // en unsigned char
+    // 2) recupérer la longueur avec un fread(buffer,0,bytes,dialogue)
+    // si > 0 alors fficher
+
+    // Traitement des réquêtes POST et GET : adapté du serveur web C minimal
+    // le traitement de la requêtes post a été rajouté.
+    if (strcasecmp(cmd, "GET") == 0 || strcasecmp(cmd, "POST") == 0)
+    {
+        int code = CODE_OK;
+        struct stat fstat;
+        sprintf(path, "%s%s", WEB_DIR, page);
+        if (stat(path, &fstat) != 0 || !S_ISREG(fstat.st_mode))
+        {
+            sprintf(path, "%s/%s", WEB_DIR, PAGE_NOTFOUND);
+            code = CODE_NOTFOUND;
+        }
+        strcpy(type, "text/html");
+        char *end = page + strlen(page);
+        if (strcmp(end - 4, ".png") == 0)
+            strcpy(type, "image/png");
+        if (strcmp(end - 4, ".jpg") == 0)
+            strcpy(type, "image/jpg");
+        if (strcmp(end - 4, ".gif") == 0)
+            strcpy(type, "image/gif");
+        fprintf(dialogue, "HTTP/1.0 %d\r\n", code);
+        fprintf(dialogue, "Server: CWeb\r\n");
+
+        fprintf(dialogue, "Content-type: %s\r\n", type);
+        fprintf(dialogue, "Content-length: %ld\r\n", fstat.st_size);
+        fprintf(dialogue, "\r\n");
+        fflush(dialogue);
+        int fd = open(path, O_RDONLY);
+        if (fd >= 0)
+        {
+            int bytes;
+            while ((bytes = read(fd, buffer, MAX_BUFFER)) > 0)
+            {
+                fwrite(buffer, 1, bytes, dialogue);
+            }
+
+            close(fd);
+        }
     }
-    
-    close(fd);
-    }
-  } 
 
     fclose(dialogue);
-    
 }
 
 // Juste pour rediriger vers la fonction gestClientWeb()
 void nouveauClientWeb(int dialogue)
 {
-   
+
     printf("Client Web connecté !\n");
     gestionClientWeb((void *)&dialogue);
 }
 
-
 /** Main procedure **/
 
-int main(void){
+int main(void)
+{
 
-
-FILE* acceuil = fopen("www/acceuil.html", "w");
-if (acceuil != NULL){
-    //fprintf(dialogue,"HTTP/1.1 200 OK\n");
-         //fprintf(dialogue,"Content-Type: text/html;\n\n");
-    fprintf(acceuil,"<!DOCTYPE html>\n<html>\n<head>");
-    fprintf(acceuil, "<title> BOTS DISPONIBLES </title></head>");
-    fprintf(acceuil, "<body><h1 color:rgb(250,128,114)> BOTS </h1><h3>Groupe: Hind MALTI & Loris AHOUASSOU</h3>");
-    for (int i=0;i<NB_BOTS;i++){
-    fprintf(acceuil,"<button onclick=\"document.location.href='formulaire.html';\"> %s </button>",ListeBot[i]);
+    FILE *acceuil = fopen("www/acceuil.html", "w");
+    if (acceuil != NULL)
+    {
+        //fprintf(dialogue,"HTTP/1.1 200 OK\n");
+        //fprintf(dialogue,"Content-Type: text/html;\n\n");
+        fprintf(acceuil, "<!DOCTYPE html>\n<html>\n<head>");
+        fprintf(acceuil, "<title> BOTS DISPONIBLES </title></head>");
+        fprintf(acceuil, "<body><h1 color:rgb(250,128,114)> BOTS </h1><h3>Groupe: Hind MALTI & Loris AHOUASSOU</h3>");
+        for (int i = 0; i < NB_BOTS; i++)
+        {
+            fprintf(acceuil, "<button onclick=\"document.location.href='formulaire.html';\"> %s </button>", ListeBot[i]);
+        }
+        fprintf(acceuil, "</body></html>");
+        fclose(acceuil);
     }
-    fprintf(acceuil, "</body></html>");
-    fclose(acceuil);
+
+    // Serveur TCP + boucle serveur
+    char *PORT_WEB = "8000";
+    int s_tcp;
+
+    s_tcp = initialisationServeurTCP(PORT_WEB);
+
+    // Initialisation du serveur
+    if (s_tcp < 0)
+    {
+        fprintf(stderr, "Initialisation du serveur impossible\n");
+        exit(-1);
+    }
+
+    // Lancement de la boucle d'ecoute
+    if (boucleServeurTCP(s_tcp, nouveauClientWeb) < 0)
+    {
+        fprintf(stderr, "Connexion avec le client impossible\n");
+        exit(-1);
+    }
+
+    close(s_tcp);
+    return 0;
 }
-
-// Serveur TCP + boucle serveur
-char *PORT_WEB = "8000";
-int s_tcp;
-
-
-s_tcp = initialisationServeurTCP(PORT_WEB);
-
-// Initialisation du serveur 
-if (s_tcp < 0)
-{
-    fprintf(stderr, "Initialisation du serveur impossible\n");
-    exit(-1);
-}
-
-// Lancement de la boucle d'ecoute 
-if (boucleServeurTCP(s_tcp, nouveauClientWeb) < 0)
-{
-    fprintf(stderr, "Connexion avec le client impossible\n");
-    exit(-1);
-}
-
-close(s_tcp);
-return 0;
-}
-
-
